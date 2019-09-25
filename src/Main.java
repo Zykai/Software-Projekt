3c6986188e97f304b91d5515dcae9437b8b42111
Tobias Palzer <palztob@inf2-18.schule.local>
2019-09-25 12:19:31 +0200

    add map + player class

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Image;
+import java.awt.Toolkit;
 import java.awt.image.BufferedImage;
  
+import javax.swing.JButton;
 //from   w ww  .j  a v a  2  s  .c  o m
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -23,11 +27,11 @@ public class Main extends JPanel {
  	 super();
  	 BufferedImage img = null;
  	 try {
-     	 img = ImageIO.read(new File("res/car.jpg"));
+     	 img = ImageIO.read(new File("res/held.png"));
  	 } catch (IOException e) {
      	 System.out.println(e.getMessage());
  	 }
- 	 image = img.getScaledInstance(150, 150, Image.SCALE_DEFAULT);
+ 	 image = img.getScaledInstance(50, 50, Image.SCALE_DEFAULT);
  }
  
  public void update(float deltaTime){
@@ -41,26 +45,29 @@ public class Main extends JPanel {
     g.setColor (Color.green);
     g.fillRect(150, 150 + (int) (50 * Math.sin(System.currentTimeMillis() / 1000.0)),200, 50);
  
-    g.drawImage(image, 40, 350,null);
+    g.drawImage(image, 0, 0,null);
  
     float fps = (float) (1000.0/deltaTime);
     g.setFont(new Font("TimesRoman", Font.PLAIN, 30));
     g.drawString(String.format("%.2f", fps),30, 30);
+    Toolkit.getDefaultToolkit().sync();
  }
  
  public static void main(String[] args) {
    JFrame frame = new JFrame();
-   frame.add(new Main());
+   JPanel main = new Main();
+   frame.add(main);
    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-   frame.setBounds(20,20, 500,500);
-    frame.setVisible(true);
-    
-    long time = System.currentTimeMillis();
- 
- 
+   frame.setBounds(20,20, 1000,750);
+   frame.setLocationRelativeTo(null);
+   frame.setVisible(true);
+   long time = System.currentTimeMillis();
+   frame.remove(main);
+   
+   frame.add(main);
     while(true){
         try {
-            //Thread.sleep(16);
+            Thread.sleep(16);
         } catch (Exception e) {
             //TODO: handle exception
         }
