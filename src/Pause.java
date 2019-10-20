330443fa30937a37ed2b290f18ab9e0497034282
joshkie <joshua.kieburgrg@gmail,com>
2019-10-20 10:14:15 +0200

    Update Pause
    Pause fast fertig

--- src/Pause.java
+++ src/Pause.java
@@ -1,13 +1,15 @@
+import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+
 import javax.imageio.ImageIO;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-
-import Player.Player;
 
 public class Pause {
 	
@@ -17,10 +19,17 @@ public class Pause {
 	private JButton backToMenu;
 	private JButton back;
 	private JButton exitGame;
+	private static int XOFFSET = 0, YOFFSET = 0;
+	
+	private int width, height;
+	public static Font cursive = new Font("Yu Gothic", Font.ITALIC, 26);
+    public static Font font = new Font("TimesRoman", Font.PLAIN, 26);
+    public static Font bigFont = new Font("TimesRoman", Font.PLAIN, 52);
+    public static Color textColor = new Color(204, 204, 204);
 	
 	static{
         try {
-            //pauseBackground = ImageIO.read(new File("res/dsui/menu_only_bg.png"));
+            pauseBackground = ImageIO.read(new File("res/dsui/menu_only_bg.png"));
         } catch (Exception e) {
             System.out.println(e.getMessage());
             System.exit(0);
@@ -53,16 +62,28 @@ public class Pause {
 		pauseP.add(back);
 		pauseP.add(exitGame);
 		
+		width = (int)(pauseBackground.getWidth(null) * 1.5);
+        height = (int)(pauseBackground.getHeight(null) * 1.5);
+		
+		
 		
 	}
 
     
     public void draw(Graphics g){
-    	g.drawRect(0,0,100, 100);
-        //g.translate(XOFFSET, YOFFSET);
+        g.translate(XOFFSET, YOFFSET);
 
-        //g.drawImage(pauseBackground, 0, 0, width, height, null);
-
+        g.drawImage(pauseBackground, 0, 0, width, height, null);
+        g.setColor(textColor);
+        g.setFont(bigFont);
+        g.drawString("Pause", 600, 150);
+        g.setFont(font);
+        g.drawRect(560,180,200, 50);
+        g.drawRect(560,250,200, 50);
+        g.drawRect(560,320,200, 50);
+        g.drawString("Continue", 600, 210);
+        g.drawString("Exit Game", 600, 280);
+        g.drawString("Back To Menu ", 590, 350);
         }
 
 }
